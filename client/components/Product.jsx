7b7bdbd391e49b71dda6e6f225363082c03cc867
justintsugranes <justintsugranes@proton.me>
2023-01-06 17:04:33 -0500

    add product to slug.js

--- client/components/Product.jsx
+++ client/components/Product.jsx
@@ -1,31 +1,25 @@
+import React from 'react'
 import Link from 'next/link'
 
-// Import the `urlFor` function from the `lib/client` module
 import { urlFor } from '../lib/client'
 
-// This is a functional component that receives a `product` object as props
-const Product = ({ product: { image, name, slug, price } }) => (
-  // Return the JSX for rendering the product card
-  <div>
-    {/* Render a Link component that navigates to the product page for the current product */}
-    <Link href={`/products/${slug.current}`}>
-      <div className="product-card">
-        {/* Render the image for the product, using the `urlFor` function to generate the URL */}
-        <img
-          src={urlFor(image && image[0])}
-          width={250}
-          height={250}
-          alt="product"
-          className="product-image"
-        />
-        {/* Render the name of the product */}
-        <p className="product-name">{name}</p>
-        {/* Render the price of the product */}
-        <p className="product-price"> ${price}</p>
-      </div>
-    </Link>
-  </div>
-)
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
 
-// Export the Product component
 export default Product
