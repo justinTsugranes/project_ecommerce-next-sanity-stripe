68b0e8e71d70ded8b7a613c8075ba824b82d31b5
justintsugranes <justintsugranes@proton.me>
2023-01-06 07:22:15 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { Product, HeroBanner, FooterBanner } from '../components'
+import { client } from '../lib/client'
+
+const Home = ({ products, bannerData }) => {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>High quality headphones and speakers</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product product={product} />
+        ))}
+      </div>
+      <FooterBanner />
+    </>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "product"]`
+  const products = await client.fetch(query)
+  const bannerQuery = `*[_type == "banner"]`
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData },
+  }
+}
+
+export default Home
