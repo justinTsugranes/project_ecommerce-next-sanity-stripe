ae2e906ed8424af4a69d2a060eee8c8e181b082a
justintsugranes <justintsugranes@proton.me>
2023-01-07 07:46:37 -0500

    add subtotal to cart

--- client/context/StateContext.jsx
+++ client/context/StateContext.jsx
@@ -6,30 +6,35 @@ const Context = createContext()
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false)
   const [cartItems, setCartItems] = useState([])
-  const [totalPrice, setTotalPrice] = useState()
+  const [totalPrice, setTotalPrice] = useState(0)
   const [totalQuantities, setTotalQuantities] = useState(0)
   const [qty, setQty] = useState(1)
 
   const onAdd = (product, quantity) => {
-    const productInCart = cartItems.find((x) => x.id === product.id)
-    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
-    setTotalQuantities(
-      (prevTotalQuantities) => prevTotalQuantities + 1 * quantity,
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id,
     )
 
-    if (productInCart) {
-      const updatedCartItems = cartItems.map((x) => {
-        if (x.id === product.id) {
-          return { ...x, quantity: x.quantity + quantity }
-        }
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          }
       })
 
       setCartItems(updatedCartItems)
     } else {
       product.quantity = quantity
+
       setCartItems([...cartItems, { ...product }])
     }
-    toast.success(`${qty} ${product.name} added to cart`)
+
+    toast.success(`${qty} ${product.name} added to the cart.`)
   }
 
   const incQty = () => {
@@ -43,52 +48,6 @@ export const StateContext = ({ children }) => {
     })
   }
 
-  // useEffect(() => {
-  //   const newTotal = cart.reduce(
-  //     (acc, item) => acc + item.price * item.quantity,
-  //     0,
-  //   )
-  //   setTotal(newTotal)
-  // }, [cart])
-
-  // const [cart, setCart] = useState([])
-  // const [total, setTotal] = useState(0)
-
-  // const addToCart = (product) => {
-  //   const item = cart.find((item) => item.id === product.id)
-  //   if (item) {
-  //     toast.error('This item is already in your cart')
-  //     return
-  //   }
-  //   setCart([...cart, { ...product, quantity: 1 }])
-  // }
-
-  // const removeFromCart = (id) => {
-  //   setCart(cart.filter((item) => item.id !== id))
-  // }
-
-  // const increment = (id) => {
-  //   const newCart = [...cart]
-  //   const index = newCart.findIndex((item) => item.id === id)
-  //   newCart[index].quantity++
-  //   setCart(newCart)
-  // }
-
-  // const decrement = (id) => {
-  //   const newCart = [...cart]
-  //   const index = newCart.findIndex((item) => item.id === id)
-  //   newCart[index].quantity--
-  //   setCart(newCart)
-  // }
-
-  // useEffect(() => {
-  //   const newTotal = cart.reduce(
-  //     (acc, item) => acc + item.price * item.quantity,
-  //     0,
-  //   )
-  //   setTotal(newTotal)
-  // }, [cart])
-
   return (
     <Context.Provider
       value={{
@@ -101,10 +60,6 @@ export const StateContext = ({ children }) => {
         decQty,
         onAdd,
         setShowCart,
-        // addToCart,
-        // removeFromCart,
-        // increment,
-        // decrement,
       }}
     >
       {children}
