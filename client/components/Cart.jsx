0d544e61f62c4cf653feb29bafd0d739e174447e
justintsugranes <justintsugranes@proton.me>
2023-01-07 08:20:35 -0500

    add cart logic

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -14,8 +14,13 @@ import { urlFor } from '../lib/client'
 
 const Cart = () => {
   const cartRef = useRef()
-  const { totalPrice, totalQuantities, cartItems, setShowCart } =
-    useStateContext()
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuantity,
+  } = useStateContext()
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -64,19 +69,29 @@ const Cart = () => {
                   <div className="flex bottom">
                     <div>
                       <p className="quantity-desc">
-                        <span className="minus" onClick="">
+                        <span
+                          className="minus"
+                          onClick={() =>
+                            toggleCartItemQuantity(item._id, 'dec')
+                          }
+                        >
                           <AiOutlineMinus />
                         </span>
                         <span className="num" onClick="">
-                          1
+                          {item.quantity}
                         </span>
-                        <span className="plus" onClick="">
+                        <span
+                          className="plus"
+                          onClick={() =>
+                            toggleCartItemQuantity(item._id, 'inc')
+                          }
+                        >
                           <AiOutlinePlus />
                         </span>
                       </p>
                     </div>
 
-                    <button type="button" className="remove-item" onClick="">
+                    <button type="button" className="remove-item" onClick={''}>
                       <TiDeleteOutline />
                     </button>
                   </div>
@@ -90,6 +105,11 @@ const Cart = () => {
             <div className="total">
               <h3>Subtotal:</h3>
               <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick="">
+                Pay with Stripe
+              </button>
             </div>
           </div>
         )}
--- client/context/StateContext.jsx
+++ client/context/StateContext.jsx
@@ -10,15 +10,21 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState(0)
   const [qty, setQty] = useState(1)
 
+  let foundProduct
+  let index
+
   const onAdd = (product, quantity) => {
+    // Check if the product is already in the cart
     const checkProductInCart = cartItems.find(
       (item) => item._id === product._id,
     )
 
+    // Update the total price and quantity
     setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
 
     if (checkProductInCart) {
+      // If the product is already in the cart, update the quantity of that product
       const updatedCartItems = cartItems.map((cartProduct) => {
         if (cartProduct._id === product._id)
           return {
@@ -29,19 +35,61 @@ export const StateContext = ({ children }) => {
 
       setCartItems(updatedCartItems)
     } else {
+      // If the product is not in the cart, add it to the cart with the specified quantity
       product.quantity = quantity
 
       setCartItems([...cartItems, { ...product }])
     }
 
+    // Show a toast message indicating that the product was added to the cart
     toast.success(`${qty} ${product.name} added to the cart.`)
   }
 
+  const toggleCartItemQuantity = (id, quantity) => {
+    // Find the product in the cart
+    const foundProduct = cartItems.find((item) => item._id === id)
+
+    // Find the index of the product in the cart
+    const index = cartItems.findIndex((product) => product._id === id)
+
+    // Create a copy of the cart items
+    let updatedCart = [...cartItems]
+
+    if (quantity === 'inc') {
+      // Increment the quantity of the found product
+      updatedCart[index] = {
+        ...foundProduct,
+        quantity: foundProduct.quantity + 1,
+      }
+
+      // Increase the total price and quantity
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
+    } else if (quantity === 'dec') {
+      // Decrement the quantity of the found product if it's greater than 1
+      if (foundProduct.quantity > 1) {
+        updatedCart[index] = {
+          ...foundProduct,
+          quantity: foundProduct.quantity - 1,
+        }
+
+        // Decrease the total price and quantity
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
+      }
+    }
+
+    // Update the cart items
+    setCartItems(updatedCart)
+  }
+
   const incQty = () => {
+    // Increment the quantity value by 1
     setQty((prevQty) => prevQty + 1)
   }
 
   const decQty = () => {
+    // Decrement the quantity value by 1, but prevent it from going below 1
     setQty((prevQty) => {
       if (prevQty - 1 < 1) return 1
       return (prevQty = 1)
@@ -60,6 +108,7 @@ export const StateContext = ({ children }) => {
         decQty,
         onAdd,
         setShowCart,
+        toggleCartItemQuantity,
       }}
     >
       {children}
